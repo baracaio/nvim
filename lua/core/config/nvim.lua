6ad56e949a087b0307cf434bf74c34914457076b
Caio Baracat <baracaio@mbp-de-caio.lan>
2024-10-15 14:06:12 -0300

    add lazy

--- lua/core/config/nvim.lua
+++ lua/core/config/nvim.lua
@@ -40,6 +40,35 @@ vim.opt.inccommand = 'split'
 vim.opt.cursorline = true
 vim.opt.tabstop = 4
 vim.opt.shiftwidth = 4
+vim.o.linebreak = true
+vim.o.autoindent = true
+vim.o.softtabstop = 4
+vim.o.expandtab = true
 
 -- Minimal number of screen lines to keep above and below the cursor.
 vim.opt.scrolloff = 20
+vim.o.wrap = false -- Display lines as one long line (default: true)
+vim.o.sidescrolloff = 8 -- Minimal number of screen columns either side of cursor if wrap is `false` (default: 0)
+
+vim.o.whichwrap = 'bs<>[]hl' -- Which "horizontal" keys are allowed to travel to prev/next line (default: 'b,s')
+
+
+vim.o.showmode = false -- We don't need to see things like -- INSERT -- anymore (default: true)
+vim.opt.termguicolors = true -- Set termguicolors to enable highlight groups (default: false)
+
+vim.o.numberwidth = 4 -- Set number column width to 2 {default 4} (default: 4)
+vim.o.swapfile = false -- Creates a swapfile (default: true)
+vim.o.smartindent = true -- Make indenting smarter again (default: false)
+vim.o.showtabline = 2 -- Always show tabs (default: 1)
+vim.o.backspace = 'indent,eol,start' -- Allow backspace on (default: 'indent,eol,start')
+vim.o.pumheight = 10 -- Pop up menu height (default: 0)
+vim.o.conceallevel = 0 -- So that `` is visible in markdown files (default: 1)
+vim.o.fileencoding = 'utf-8' -- The encoding written to a file (default: 'utf-8')
+vim.o.cmdheight = 1 -- More space in the Neovim command line for displaying messages (default: 1)
+vim.o.backup = false -- Creates a backup file (default: false)
+vim.o.writebackup = false -- If a file is being edited by another program (or was written to file while editing with another program), it is not allowed to be edited (default: true)
+vim.o.completeopt = 'menuone,noselect' -- Set completeopt to have a better completion experience (default: 'menu,preview')
+vim.opt.shortmess:append 'c' -- Don't give |ins-completion-menu| messages (default: does not include 'c')
+vim.opt.iskeyword:append '-' -- Hyphenated words recognized by searches (default: does not include '-')
+vim.opt.formatoptions:remove { 'c', 'r', 'o' } -- Don't insert the current comment leader automatically for auto-wrapping comments using 'textwidth', hitting <Enter> in insert mode, or hitting 'o' or 'O' in normal mode. (default: 'croql')
+vim.opt.runtimepath:remove '/usr/share/vim/vimfiles'
--- lua/go/plugins/bufferline.lua
+++ lua/go/plugins/bufferline.lua
@@ -1,5 +1,9 @@
 return {
     'akinsho/bufferline.nvim',
+    dependencies = {
+        'moll/vim-bbye',
+        'nvim-tree/nvim-web-devicons',
+      },
     event = 'VeryLazy',
     keys = {
         { '<leader>bp', '<Cmd>BufferLineTogglePin<CR>', desc = 'Toggle Pin' },
@@ -17,7 +21,6 @@ return {
     opts = {
         options = {
             diagnostics = 'nvim_lsp',
-            always_show_bufferline = false,
             offsets = {
                 {
                     filetype = 'neo-tree',
@@ -26,7 +29,54 @@ return {
                     text_align = 'left',
                 },
             },
+
+            mode = 'buffers', -- set to "tabs" to only show tabpages instead
+            themable = true, -- allows highlight groups to be overriden i.e. sets highlights as default
+            numbers = 'none', -- | "ordinal" | "buffer_id" | "both" | function({ ordinal, id, lower, raise }): string,
+            close_command = 'Bdelete! %d', -- can be a string | function, see "Mouse actions"
+            buffer_close_icon = '✗',
+            close_icon = '✗',
+            path_components = 1, -- Show only the file name without the directory
+            modified_icon = '●',
+            left_trunc_marker = '',
+            right_trunc_marker = '',
+            max_name_length = 30,
+            max_prefix_length = 30, -- prefix used when a buffer is de-duplicated
+            tab_size = 21,
+            color_icons = true,
+            show_buffer_icons = true,
+            show_buffer_close_icons = true,
+            show_close_icon = true,
+            persist_buffer_sort = true, -- whether or not custom sorted buffers should persist
+            separator_style = { '│', '│' }, -- | "thick" | "thin" | { 'any', 'any' },
+            enforce_regular_tabs = true,
+            always_show_bufferline = true,
+            show_tab_indicators = false,
+            indicator = {
+            -- icon = '▎', -- this should be omitted if indicator style is not 'icon'
+            style = 'none', -- Options: 'icon', 'underline', 'none'
+            },
+            icon_pinned = '󰐃',
+            minimum_padding = 1,
+            maximum_padding = 5,
+            maximum_length = 15,
+            sort_by = 'insert_at_end',
         
+        },
+
+        highlights = {
+            separator = {
+                fg = '#434C5E',
+            },
+            buffer_selected = {
+                bold = true,
+                italic = false,
+            },
+            -- separator_selected = {},
+            -- tab_selected = {},
+            -- background = {},
+            -- indicator_selected = {},
+            -- fill = {},
         },
     },
     config = function(_, opts)
