878b96f8ab08e17583fe8f495fcba5d568dff7e5
Caio Baracat <baracaio@mbp-de-caio.lan>
2024-10-15 19:02:40 -0300

    remove forced tabs and add some options

--- lua/lz/config/lazy.lua
+++ lua/lz/config/lazy.lua
@@ -22,7 +22,7 @@ require("lazy").setup({
 
     { import = "lazyvim.plugins.extras.lang.go" },
 
-    { import = "lazyvim.plugins" },
+    { import = "lz.plugins" },
   },
   defaults = {
     -- By default, only LazyVim plugins will be lazy-loaded. Your custom plugins will load during startup.
--- lua/lz/config/options.lua
+++ lua/lz/config/options.lua
@@ -1,3 +1,73 @@
--- Options are automatically loaded before lazy.nvim startup
--- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
--- Add any additional options here
+vim.g.have_nerd_font = true
+
+vim.opt.number = true
+vim.opt.relativenumber = true
+
+vim.opt.mouse = "a"
+
+vim.opt.showmode = false
+
+-- sync clipboard between OS and Neovim.
+vim.schedule(function()
+  vim.opt.clipboard = "unnamedplus"
+end)
+
+vim.opt.breakindent = true
+
+-- save undo history
+vim.opt.undofile = true
+
+-- Case-insensitive searching UNLESS \C or one or more capital letters in the search term
+vim.opt.ignorecase = true
+vim.opt.smartcase = true
+
+vim.opt.signcolumn = "yes"
+
+vim.opt.updatetime = 250
+
+vim.opt.timeoutlen = 300
+
+vim.opt.splitright = true
+vim.opt.splitbelow = true
+
+-- Sets how neovim will display certain whitespace characters in the editor.
+vim.opt.list = true
+vim.opt.listchars = { tab = "» ", trail = "·", nbsp = "␣" }
+
+-- Preview substitutions live, as you type!
+vim.opt.inccommand = "split"
+
+vim.opt.cursorline = true
+vim.opt.tabstop = 4
+vim.opt.shiftwidth = 4
+vim.o.linebreak = true
+vim.o.autoindent = true
+vim.o.softtabstop = 4
+vim.o.expandtab = true
+
+-- Minimal number of screen lines to keep above and below the cursor.
+vim.opt.scrolloff = 20
+vim.o.wrap = false -- Display lines as one long line (default: true)
+vim.o.sidescrolloff = 8 -- Minimal number of screen columns either side of cursor if wrap is `false` (default: 0)
+
+vim.o.whichwrap = "bs<>[]hl" -- Which "horizontal" keys are allowed to travel to prev/next line (default: 'b,s')
+
+vim.o.showmode = false -- We don't need to see things like -- INSERT -- anymore (default: true)
+vim.opt.termguicolors = true -- Set termguicolors to enable highlight groups (default: false)
+
+vim.o.numberwidth = 4 -- Set number column width to 2 {default 4} (default: 4)
+vim.o.swapfile = false -- Creates a swapfile (default: true)
+vim.o.smartindent = true -- Make indenting smarter again (default: false)
+vim.o.showtabline = 2 -- Always show tabs (default: 1)
+vim.o.backspace = "indent,eol,start" -- Allow backspace on (default: 'indent,eol,start')
+vim.o.pumheight = 10 -- Pop up menu height (default: 0)
+vim.o.conceallevel = 0 -- So that `` is visible in markdown files (default: 1)
+vim.o.fileencoding = "utf-8" -- The encoding written to a file (default: 'utf-8')
+vim.o.cmdheight = 1 -- More space in the Neovim command line for displaying messages (default: 1)
+vim.o.backup = false -- Creates a backup file (default: false)
+vim.o.writebackup = false -- If a file is being edited by another program (or was written to file while editing with another program), it is not allowed to be edited (default: true)
+vim.o.completeopt = "menuone,noselect" -- Set completeopt to have a better completion experience (default: 'menu,preview')
+vim.opt.shortmess:append("c") -- Don't give |ins-completion-menu| messages (default: does not include 'c')
+vim.opt.iskeyword:append("-") -- Hyphenated words recognized by searches (default: does not include '-')
+vim.opt.formatoptions:remove({ "c", "r", "o" }) -- Don't insert the current comment leader automatically for auto-wrapping comments using 'textwidth', hitting <Enter> in insert mode, or hitting 'o' or 'O' in normal mode. (default: 'croql')
+vim.opt.runtimepath:remove("/usr/share/vim/vimfiles")
